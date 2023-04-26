526d5b0aa523988748bc305d378d6fc200609e5f
Hammak <saint3543jakuzi@gmail.com>
2023-04-27 02:10:56 +0300

    created and added basic stuff to EnemyCore.cs

---
+++ Assets/Scripts/EnemyCore.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class EnemyCore : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 1;
+    private int _currentHealth;
+
+    [SerializeField] private int attackDamage = 2;
+
+    [SerializeField] private float attackCooldown = 2f;
+    private float _timer = 0f;
+
+    [SerializeField] private Transform player;
+    private Animator _animator;
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
